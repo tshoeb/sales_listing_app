d770629d3a4def1c05d9f78294f9b40925868d9b
Srinjoy Chakravarty <chaxz93@gmail.com>
2012-12-09 03:44:03 +0300

    mailer model added

---
+++ config/initializers/setup_mailer.rb
@@ -0,0 +1,1 @@
+ActionMailer::Base.smtp_settings = { :address => "smtp.gmail.com", :port => 587, :user_name => "<your username>", :password => "<your password>", :authentication => "plain", :enable_starttls_auto => true }
